94955cdbf15ac7df599f6a7f071192e6cc3ce9a5
Chris Partridge <cpartridge@docero.com>
2019-08-20 16:11:28 -0400

    added app section

--- wp-content/themes/selus/main.js
+++ wp-content/themes/selus/main.js
@@ -15,3 +15,18 @@ jQuery('document').ready(function () {
 	}, 750);
 });
 
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fadeIn").each(function() {
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      
+      if (objectBottom < windowBottom) {
+        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      } else {
+        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+      }
+    });
+  }).scroll();
+});
+
--- wp-content/themes/selus/templates/home/intro-section.php
+++ wp-content/themes/selus/templates/home/intro-section.php
@@ -1,12 +1,13 @@
 <?php
-	
+	$mainBG = get_field('main_background_image');
+	$introContent = get_field('intro_text');
 ?>
 
-<div class="col-12 intro-section-container">
+<div class="col-12 intro-section-container" style="background-image: url(<?php echo $mainBG['url']; ?>);">
 	<div class="container">
 		<div class="row intro-content-wrapper">
 			<div class="col-12 intro-content-container">
-				<?php echo the_content(); ?>
+				<?php echo $introContent; ?>
 			</div>
 		</div>
 	</div>
